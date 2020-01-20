e9a8214237429c4f904ab05628756989bc4dfd3c
Vinicius Thiago <vtls@cin.ufpe.br>
2020-01-20 19:11:08 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		//git remote add origin
+		//git pull origin master
+		System.out.println("Hello World!");
+		
+	}
+
+}
