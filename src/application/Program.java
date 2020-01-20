77409deb117c37af6536c505a6e3d1b01e228cd6
Vinicius Thiago <vtls@cin.ufpe.br>
2020-01-20 19:18:51 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
 		//git remote add origin
 		//git pull origin master
-		System.out.println("Hello World!");
+
+		Position pos = new Position(2, 3);
+		
+		System.out.println(pos);
 		
 	}
 
