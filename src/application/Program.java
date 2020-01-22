1c59c935c5c8af2a8a47ce5f29488e440e38e232
Vinicius Thiago <vtls@cin.ufpe.br>
2020-01-22 19:30:25 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
@@ -8,9 +12,25 @@ public class Program {
 
 		//git remote add origin
 		//git pull origin master
-
+		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while(true)
+		{
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece =  chessMatch.performChessMove(source, target);
+		}
+		
 	}
 
 }
