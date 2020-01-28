2d2d1a2cf8cbb27d637db3d8df092dff254fd34d
Vinicius Thiago <vtls@cin.ufpe.br>
2020-01-28 20:39:38 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,7 +21,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true)
+		while(!chessMatch.getCheckMate())
 		{
 			try 
 			{
@@ -53,6 +53,9 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 
 }
